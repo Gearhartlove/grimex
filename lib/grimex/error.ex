b7119d9e23744336420088e9439a5dafff73b8dd
Kristoff Finley <kristoffgearhartfinley@gmail.com>
2026-04-27 07:52:43 -0600

    Refactor error to raise

--- lib/grimex/error.ex
+++ lib/grimex/error.ex
@@ -1,5 +1,5 @@
 defmodule Grimex.Error do
   def report(reason, %{line: line, where: where}) do
-    IO.puts(:stderr, "[line #{line}] Error #{where}: #{reason}")
+    raise "[line #{line}] Error #{where}: #{reason}"
   end
 end
